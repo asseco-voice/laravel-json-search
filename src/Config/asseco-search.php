a22596cc12d9057a1eb572b94a4e876ed1522201
Marko Dupor <marko.dupor@asseco-see.hr>
2020-08-17 15:52:06 +0200

    Fixed config key

--- src/Config/asseco-search.php
+++ src/Config/asseco-search.php
@@ -5,5 +5,5 @@ return [
      * If favorites are enabled, migrations and routes for this feature
      * will be enabled as well. Defaults to false
      */
-    'search_favorites' => env('SEARCH_FAVORITES_ENABLED', false) === true,
+    'search_favorites_enabled' => env('SEARCH_FAVORITES_ENABLED', false) === true,
 ];
