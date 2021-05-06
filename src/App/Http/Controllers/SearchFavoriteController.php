fefa39a8dcb3c005d7930ef0ca0254c9a6876a3c
Marko Dupor <Norgul@users.noreply.github.com>
2021-05-06 08:32:11 +0000

    Apply fixes from StyleCI

--- src/App/Http/Controllers/SearchFavoriteController.php
+++ src/App/Http/Controllers/SearchFavoriteController.php
@@ -60,7 +60,7 @@ class SearchFavoriteController extends Controller
      * Update the specified resource in storage.
      *
      * @param SearchFavoriteRequest $request
-     * @param SearchFavorite $searchFavorite
+     * @param SearchFavorite        $searchFavorite
      *
      * @return JsonResponse
      */
@@ -76,9 +76,9 @@ class SearchFavoriteController extends Controller
      *
      * @param SearchFavorite $searchFavorite
      *
-     * @return JsonResponse
      * @throws Exception
      *
+     * @return JsonResponse
      */
     public function destroy(SearchFavorite $searchFavorite): JsonResponse
     {
