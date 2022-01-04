93cc7e678c9be196924dc6e11103e96ba341335f
Marko Dupor <marko.dupor@asseco-see.hr>
2022-01-04 11:53:05 +0100

    Controller logic to model

--- src/App/Http/Controllers/SearchController.php
+++ src/App/Http/Controllers/SearchController.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 namespace Asseco\JsonSearch\App\Http\Controllers;
 
 use Asseco\JsonSearch\App\Http\Requests\SearchRequest;
+use Asseco\JsonSearch\App\Models\Search;
 use Exception;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Query\Builder;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Str;
 
 class SearchController extends Controller
 {
@@ -17,109 +15,46 @@ class SearchController extends Controller
      * Display a listing of the resource.
      *
      * @param SearchRequest $request
-     * @param string        $modelName
-     *
-     * @throws Exception
+     * @param string $modelName
      *
      * @return JsonResponse
+     * @throws Exception
+     *
      */
     public function index(SearchRequest $request, string $modelName): JsonResponse
     {
-        $model = $this->extractModelClass($modelName);
-
-        $query = $model->search($request->except(['append', 'scopes']));
-
-        foreach ($request->get('scopes', []) as $scope) {
-            $query->{$scope}();
-        }
-
-        $resolved = $query->get();
-
-        return response()->json(
-            $resolved->append($request->get('append', []))
-        );
+        return response()->json(Search::get($request, $modelName));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param SearchRequest $request
-     * @param string        $modelName
-     *
-     * @throws Exception
+     * @param string $modelName
      *
      * @return JsonResponse
+     * @throws Exception
+     *
      */
     public function update(SearchRequest $request, string $modelName): JsonResponse
     {
-        $model = $this->extractModelClass($modelName);
-
-        $search = $model->search($request->except('update'));
-
-        if (!$request->has('update')) {
-            throw new Exception('Missing update parameters');
-        }
-
-        $search->update($request->update);
-
-        return response()->json($search->get());
+        return response()->json(Search::update($request, $modelName));
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param SearchRequest $request
-     * @param string        $modelName
-     *
-     * @throws Exception
+     * @param string $modelName
      *
      * @return JsonResponse
+     * @throws Exception
+     *
      */
     public function destroy(SearchRequest $request, string $modelName): JsonResponse
     {
-        $model = $this->extractModelClass($modelName);
-        $foundModels = $model->search($request->all())->get();
-
-        // This can be executed as a single query, but then we are left without
-        // deleted event triggers. If there is a better way, I'm all ears.
-        foreach ($foundModels as $foundModel) {
-            /**
-             * @var $foundModel Model
-             */
-            $foundModel->delete();
-        }
+        Search::delete($request, $modelName);
 
         return response()->json();
-    }
-
-    /**
-     * @param string $modelName
-     *
-     * @throws Exception
-     *
-     * @return Model|Builder
-     */
-    protected function extractModelClass(string $modelName)
-    {
-        $mapping = config('asseco-search.model_mapping');
-
-        if (array_key_exists($modelName, $mapping)) {
-            $model = $mapping[$modelName];
-
-            return is_callable($model) ? $model() : new $model();
-        }
-
-        $namespaces = config('asseco-search.models_namespaces');
-
-        $formattedModelName = Str::studly(Str::singular($modelName));
-
-        foreach ($namespaces as $namespace) {
-            $model = "$namespace\\$formattedModelName";
-            if (class_exists($model)) {
-                return new $model();
-            }
-        }
-
-        throw new Exception("Model $modelName not found. Check the configuration for namespace mappings.");
     }
 }
---
+++ src/App/Models/Search.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Asseco\JsonSearch\App\Models;
+
+use Asseco\JsonSearch\App\Http\Requests\SearchRequest;
+use Exception;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Str;
+
+class Search
+{
+    /**
+     * @param SearchRequest $request
+     * @param string $modelName
+     * @return \Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
+     * @throws Exception
+     */
+    public static function get(SearchRequest $request, string $modelName)
+    {
+        $model = self::extractModelClass($modelName);
+
+        $query = $model->search($request->except(['append', 'scopes']));
+
+        foreach ($request->get('scopes', []) as $scope) {
+            $query->{$scope}();
+        }
+
+        $resolved = $query->get();
+
+        return $resolved->append($request->get('append', []));
+    }
+
+    /**
+     * @param SearchRequest $request
+     * @param string $modelName
+     * @return \Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
+     * @throws Exception
+     */
+    public static function update(SearchRequest $request, string $modelName)
+    {
+        $model = self::extractModelClass($modelName);
+
+        $search = $model->search($request->except('update'));
+
+        if (!$request->has('update')) {
+            throw new Exception('Missing update parameters');
+        }
+
+        $search->update($request->update);
+
+        return $search->get();
+    }
+
+    /**
+     * @param SearchRequest $request
+     * @param string $modelName
+     * @throws Exception
+     */
+    public static function delete(SearchRequest $request, string $modelName)
+    {
+        $model = self::extractModelClass($modelName);
+        $foundModels = $model->search($request->all())->get();
+
+        // This can be executed as a single query, but then we are left without
+        // deleted event triggers. If there is a better way, I'm all ears.
+        foreach ($foundModels as $foundModel) {
+            /**
+             * @var $foundModel Model
+             */
+            $foundModel->delete();
+        }
+    }
+
+    /**
+     * @param string $modelName
+     *
+     * @return Model|Builder
+     * @throws Exception
+     *
+     */
+    protected static function extractModelClass(string $modelName)
+    {
+        $mapping = config('asseco-search.model_mapping');
+
+        if (array_key_exists($modelName, $mapping)) {
+            $model = $mapping[$modelName];
+
+            return is_callable($model) ? $model() : new $model();
+        }
+
+        $namespaces = config('asseco-search.models_namespaces');
+
+        $formattedModelName = Str::studly(Str::singular($modelName));
+
+        foreach ($namespaces as $namespace) {
+            $model = "$namespace\\$formattedModelName";
+            if (class_exists($model)) {
+                return new $model();
+            }
+        }
+
+        throw new Exception("Model $modelName not found. Check the configuration for namespace mappings.");
+    }
+}
