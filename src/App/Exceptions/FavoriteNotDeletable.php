f6196544723c5d4eb2b9f649ee9cebf9c714d1ad
Marko Dupor <marko.dupor@asseco-see.hr>
2022-09-13 12:07:19 +0200

    Quickfix

--- src/App/Exceptions/FavoriteNotDeletable.php
+++ src/App/Exceptions/FavoriteNotDeletable.php
@@ -4,16 +4,14 @@ namespace Asseco\JsonSearch\App\Exceptions;
 
 use Asseco\JsonSearch\App\Models\SearchFavorite;
 use Exception;
-use JetBrains\PhpStorm\Pure;
 use Throwable;
 
 class FavoriteNotDeletable extends Exception
 {
-    #[Pure]
- public function __construct(SearchFavorite $searchFavorite, int $code = 0, ?Throwable $previous = null)
- {
-     $message = "Search favorite $searchFavorite->name is set as un-deletable. Flip the switch or abort deleting.";
+    public function __construct(SearchFavorite $searchFavorite, int $code = 0, ?Throwable $previous = null)
+    {
+        $message = "Search favorite $searchFavorite->name is set as un-deletable. Flip the switch or abort deleting.";
 
-     parent::__construct($message, $code, $previous);
- }
+        parent::__construct($message, $code, $previous);
+    }
 }
